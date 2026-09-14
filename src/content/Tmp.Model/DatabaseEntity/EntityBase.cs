aad661a23c0953f5b0c46284ac7340e00a31bc8e
Rhaegar Cheng <Rhaegar.Cheng@outlook.com>
2026-09-14 18:10:25 +0800

    Fix time issue.

--- src/content/Tmp.Model/DatabaseEntity/EntityBase.cs
+++ src/content/Tmp.Model/DatabaseEntity/EntityBase.cs
@@ -12,11 +12,11 @@ public abstract class EntityBase
 
     public string? CreatedBy { get; set; }
 
-    public DateTime CreatedOn { get; set; }
+    public DateTimeOffset CreatedOn { get; set; }
 
     public string? LastModifiedBy { get; set; }
 
-    public DateTime LastModifiedOn { get; set; }
+    public DateTimeOffset LastModifiedOn { get; set; }
 
     public bool? IsDeleted { get; set; }
 }
--- src/content/Tmp.Repository/AuditSaveChangesInterceptor.cs
+++ src/content/Tmp.Repository/AuditSaveChangesInterceptor.cs
@@ -52,7 +52,7 @@ internal sealed class AuditSaveChangesInterceptor(IUserContextService userContex
             return;
         }
 
-        var now = DateTime.Now;
+        var now = DateTimeOffset.UtcNow;
         var actor = ResolveActor();
 
         foreach (var entry in context.ChangeTracker.Entries<EntityBase>())
@@ -62,6 +62,7 @@ internal sealed class AuditSaveChangesInterceptor(IUserContextService userContex
                 case EntityState.Added:
                     entry.Entity.CreatedOn = now;
                     entry.Entity.CreatedBy ??= actor;
+                    entry.Entity.IsDeleted = false;
                     break;
                 case EntityState.Modified:
                     entry.Entity.LastModifiedOn = now;
